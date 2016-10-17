2456a8c7718a26a71a3bd024d0864e4d5deccb0b
Andrew Beers <andybeers@gmail.com>
2016-10-17 14:11:49 -0700

    learning testing and modules

---
+++ test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var greet = require('./greet');
+
+describe('greeter', function() {
+
+    it('greets with name', function() {
+        var greeting = greet('tester');
+        assert.equal(greeting, 'hello tester');
+    });
+
+    it('greets with "friend" when no name given', function() {
+        var greeting = greet('');
+        assert.equal(greeting, 'hello tester');
+    });
+
+});
