fba28dc92c50f52b7214988558e5ee03b1ad12a7
Waxhoya <waxhoya@gmail.com>
2016-10-17 16:06:10 -0700

    reverted to working version

--- readlineTest.js
+++ readlineTest.js
@@ -6,7 +6,9 @@ prompts.question('What is the airspeed of a swallow?', function(swallowSpeed) {
     var msg = "I don't know?"
     console.log('the prompt: '+swallowSpeed);
     if (swallowSpeed){
-        console.log('Done');
+        console.log(swallowSpeed, 'Done');
+    } else {
+        console.log("fail: empty");
     }
     process.exit();
 });
