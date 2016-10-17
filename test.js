dbf7b3ff9a2c91008ded48d211a55859d61f46ab
dratech <alfonsotech@nym.hush.com>
2016-10-17 15:25:01 -0700

    added files to test greeting function

---
+++ test.js
@@ -0,0 +1,28 @@
+//require the module
+var assert = require('assert');
+//require the greet file adn provide path to file
+var greet = require('./greet');
+
+describe ('greeter', function() {
+    //what it should do
+    it('greets with name', function() {
+        var greeting = greet('tester');
+        //run through module to check
+        assert.equal(greeting, 'hello tester');
+    });
+    
+    //what it should do if no name is provided
+    it('greets with "friend" when no name is given', function() {
+        var greeting = greet();
+        assert.equal(greeting, 'hello friend');
+    });
+    
+    //another test, just for fun!
+    describe('some other functionality', function() {
+
+        it('this other test ...', function() {
+            assert.equal(2,2);
+        });
+    });
+
+});
