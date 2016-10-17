f42b9d5c1e8ddf4ddc444b7a6e105b336c853a19
Waxhoya <waxhoya@gmail.com>
2016-10-17 15:58:18 -0700

    Created a console query with readLine

---
+++ readlineTest.js
@@ -0,0 +1,14 @@
+var rl = require('readline');
+
+var prompts = rl.createInterface(process.stdin, process.stdout);
+
+prompts.question('What is the airspeed of a swallow?', function(swallowSpeed) {
+    var msg = "I don't know?"
+    console.log('the prompt: '+swallowSpeed);
+    if (swallowSpeed){
+        console.log('Done');
+    }
+    process.exit();
+});
+
+module.exports = readLine;
