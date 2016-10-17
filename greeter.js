e33bdc0c442c82ec26d57faa83aa8125f607ade6
Gloria Guy <totallygloria@gmail.com>
2016-10-17 15:58:09 -0700

    Wrote a basic outline of three cases of saying hello: the first takes a name and logs/returns it, if no name is passed to the function, it just appends a ! to hello and logs/returns that, if there were process.argv passed to it, the last calls the function with the name scraped from the commandline, as if it was passed to the function. All three cases log the names correctly.

---
+++ greeter.js
@@ -0,0 +1,26 @@
+/** Created by Gloria on 10/17/16. **/
+
+
+if (process.argv.length > 2) {
+  // if this is called with more than just node and the script name, pass those to sayHello
+  argString = process.argv.slice(2).join(' ');
+  sayHello(argString);
+}
+
+function sayHello(name) {
+  var salutation = 'Hello';
+
+  // process the name passed in, otherwise add an !
+  if (name) {
+    console.log(salutation + ' ' + name + '!');
+    return salutation + ' ' + name + '!';
+  } else {
+    console.log(salutation + '!');
+    return salutation + '!';
+  }
+}
+
+sayHello('Erica');
+sayHello();
+
+module.exports = sayHello;
