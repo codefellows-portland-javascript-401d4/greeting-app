b8281bad9c97a8751a6d8b23bb92fd33c78184ba
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-17 17:43:29 -0700

    Format changes after setting up VSCode to satisfy linter

--- greet.js
+++ greet.js
@@ -1,12 +1,12 @@
 function greet(name) {
-    name = name || 'friend';
-    return 'Hello, ' + name;
+  name = name || 'friend';
+  return 'Hello, ' + name;
 }
 
 // If this tests true, it means we were run from the command-line
 // Otherwise, we were invoked in a 'require' statement
 if (require.main == module) {
-    console.log(greet(process.argv[2]));
+  console.log(greet(process.argv[2]));
 }
 
 module.exports = greet;
