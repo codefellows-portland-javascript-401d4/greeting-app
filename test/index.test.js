91ef31ce98926af0c7c18fa3feaeb755623f8779
Gloria Guy <totallygloria@gmail.com>
2016-10-18 15:34:02 -0700

    Passes the first index.js test - can take an empty string as input.

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,28 @@
 /** Created by Gloria on 10/18/16. **/
 
 var assert = require('assert');
+var chai = require('chai');
 var sayHello = require('../greeter');
 var cmd = require('../index');
 
+// Grab the last argument passed to this script
 var args = process.argv[(process.argv.length - 1)];
+
+// If it contains a .js, it's a path, so don't use it. Otherwise, assume it's a name.
+if (args.indexOf('.js') > -1) {
+  args = '';
+}
+
 var child = require('child_process')
-  .execSync('node greeter.js ' + '"' + args + '"');
+  .execSync('node index.js ' + '"' + args + '"');
+
 
 describe('index', function() {
-  it('greets a console user', function () {
+
+  it('greets a console user providing an empty string', function () {
     // test that arguments taken in via the command line are processed correctly
     var greeting = child.toString();
-    assert.equal(greeting, 'Hello ' + args + '!\n');
+    assert.equal(greeting, 'Hello' + args + '!\n');
   });
 
 });
