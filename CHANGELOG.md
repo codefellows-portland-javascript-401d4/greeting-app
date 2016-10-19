cec4cf044b8d586bb1d4ca0448b977c308b67cbe
Tim Combs <tim00combs@gmail.com>
2016-10-18 17:50:33 -0700

    add new tests, add date & time functionality

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,10 +3,15 @@ All changes to this project will be documented in this CHANGELOG
 
 Format is based on Keep a Changelong (http://keepachangelog.com/) and the project adheres to Semantic Versioning (http://semver.org).
 
-## [Unreleased]
+
+## [1.1.0] - 2016-10-18
+###
+- Refactored return from string into object
+
 ### Added
-- Set basic greet functionality.
-- Created conditional for greeting with name, with my name and no name.
+- Created current date functionality
+- Created current time functionality
+
 
 ## [1.0.0] - 2016-10-18
 ### Changed
@@ -16,3 +21,5 @@ Format is based on Keep a Changelong (http://keepachangelog.com/) and the projec
 - New directory structure
 - package.json
 - CHANGELOG.md
+- Set basic greet functionality.
+- Created conditional for greeting with name, with my name and no name.
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,10 +1,30 @@
-var assert = require('assert');
 var greet = require('../lib/greet');
-var chai = require('chai');
+var assert = require('chai').assert;
 //for the command line bonus
 var es = require('child_process').execSync;
 
+    //give it a name
+    //gets back 
+    //logs the current day of the week as a string
 describe('greeter', function() {
+  it('gets the current day of week', function() {
+    var greeting = greet('anyname');
+    var current = greeting.currentDay;
+    assert.match(current, /^(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)/g, 'currentDay passes the regexp');
+  });
+
+  it('gets the current time', function() {
+    var greeting = greet('anothername');
+    var current = greeting.currentTime;
+    assert.match(current, /^([1-9]|1[0-2]):([0-5]\d)\s?(AM|PM)?$/i);
+  });
+
+  it('gets the greeting', function() {
+    var fullGreeting = greet('yetanothername');
+    var greeting = fullGreeting.greeting;
+    assert.equal(greeting, 'Hello yetanothername, how you doing?');
+  });
+
   it('greets using name', function() {
     var greeting = greet('tester');
     assert.equal(greeting, 'Hello tester, how you doing?');
@@ -25,6 +45,15 @@ describe('greeter', function() {
       encoding: 'utf-8'
     }).trim();
     assert.equal(greeting, 'Hello Bob, how you doing?');
+  });
+
+  it('gets the current time with am/pm', function() {
+    var timeOfGreeting = new Date();
+    assert()
+  });
+
+  it('turn name/friend into ascii art', function() {
+
   });
 
   // compute time at greeting call
