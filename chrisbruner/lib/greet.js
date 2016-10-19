4cde1c354993f4c715ecf87e093971a9aac3abf5
Christopher Bruner <chrbruner9@gmail.com>
2016-10-18 17:46:27 -0700

    added new functionality to greet and will have you return a question after initiated

--- chrisbruner/lib/greet.js
+++ chrisbruner/lib/greet.js
@@ -1,38 +1,58 @@
-const readline = require('readline');
-var cool = require('cool-ascii-faces');
+var emoji = require('node-emoji');
+var emojiDictionary = emoji.emoji;
+var readline = require('readline');
 
 //in case does not work, you can still enter another name and this will print out
-const rl = readline.createInterface({
+
+var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    // prompt: process.argv[2] ? 'hello ' + process.argv.pop() + ' ' + cool() + '\n' : 'hello' + ' ' + cool() + '\n'
-    prompt: process.argv[2] ? 'hello ' + process.argv.pop() + '\n' : 'hello\n'  
 });
 
-function greet(name) { 
-    if (Array.isArray(name)) {
-        if (name.length < 2) {
-            return 'hello ' + name[0];
-        } else if (name.length < 3) {
-            return 'hello ' + name[0] + ' and ' + name[1];
-        } else {
-            var newString = 'hello ';
-            for (var i = 0; i < name.length; i++) {
-                if (i < (name.length - 1)) {
-                    newString += name[i] + ', ';
-                } else {
-                    newString += 'and ' + name[i];
-                };
+console.log(greet(process.argv.pop()));
+rl.question('How are you?', (answer) => {
+    console.log('I see, you\'re feeling ' + answer + '. Just so you know, I think you\'re awesome.');
+    rl.close();
+});
+
+function greetNameArray(nameArray) {
+    if (nameArray.length < 2) {
+        return 'hello ' + nameArraye[0];
+    } else if (nameArray.length < 3) {
+        return 'hello ' + nameArray[0] + ' and ' + nameArray[1];
+    } else {
+        var newString = 'hello ';
+        for (var i = 0; i < nameArray.length; i++) {
+            if (i < (nameArray.length - 1)) {
+                newString += nameArray[i] + ', ';
+            } else {
+                newString += 'and ' + nameArray[i];
             };
-            return newString;
         };
+        return newString;
     };
-    name = (name || 'friend');
-    return 'hello ' + name;
 };
 
-rl.prompt();
-rl.close(); 
+function greet(name) { 
+    var nameRegex;
+    if (typeof(name) === 'string') {
+        nameRegex = /((Chris)(topher)*)(\s)(Bruner)/g;
+        if (name.match(nameRegex)) {
+            return emoji.emojify('Yo Chris! What up buddy? Have you had your coffee today? :coffee:');
+        } else {
+            return 'hello ' + name;
+        };
+    } else if (Array.isArray(name)) {
+        return greetNameArray(name);
+    } else {
+        return 'hello friend';
+    };
+};
 
+function parting(name) {
+    name = name || 'friend';
+    return 'see you ' + name;
+};
 
-module.exports = greet;
+module.exports = { greet: greet,
+    parting: parting };
--- chrisbruner/test.js
+++ chrisbruner/test.js
@@ -1,9 +1,11 @@
 var assert = require('chai').assert;
-var greet = require('./lib/greet');
+var greet = require('./lib/greet').greet;
+var parting = require('./lib/greet').parting;
 var esync = require('child_process').execSync;
+var emoji = require('node-emoji');
+var exec = require('child_process').exec;
 
 describe('greeter', function() {
-
     it('greets with name', function() {
         //do the thing we want to test
         var greeting = greet('tester');
@@ -27,9 +29,43 @@ describe('greeter', function() {
         assert.equal(greeting, 'hello Larry, Moe, and Curly');
     });
 
-    it('this will return a prompt with your name in the console', function() {
-        var greeting = esync('node ./lib/greet.js "Jane Doe"', {encoding: 'utf-8'});
-        assert.equal(greeting, 'hello Jane Doe\n');
+    it('this will return a special greeting when I input my name (Christopher|Chris) Bruner into greet', function() {
+        var greeting = greet('Christopher Bruner');
+        var check = emoji.emojify('Yo Chris! What up buddy? Have you had your coffee today? :coffee:');
+        assert.equal(greeting, check);
+    });
+
+    it('this will check the first prompt', function() {
+        var check = esync('node ./lib/greet.js "Jane Doe"', {encoding:'utf-8'});
+        assert.equal(check, 'hello Jane Doe\nHow are you?');
+    });
+
+    it('this will check your input after the initial prompt', function(done) {
+        var child = exec('node ./lib/greet.js "Jane Doe"');
+        var loopCounter = 0;
+        child.stdout.on('data', function(data) {
+            if(loopCounter === 0) {
+                loopCounter += 1;
+                assert.equal(data, 'hello Jane Doe\n');
+            } else if (loopCounter === 1) {
+                loopCounter += 1;
+                assert.equal(data, 'How are you?');
+                child.stdin.write('crappy\n');
+            } else if (loopCounter === 2) {
+                loopCounter += 1;
+                assert.equal(data, 'I see, you\'re feeling crappy. Just so you know, I think you\'re awesome.\n');
+                done();
+            } else {
+                done();
+            }
+        });
+    });
+});
+
+describe('parting', function() {
+    it('this should test the most basic parting phrase', function() {
+        var part = parting('Chris');
+        assert.equal(part, 'see you Chris');
     })
 });
 
