0f14958cffcdff9cc182fbeb60a3160c9495559f
Nathan P. Keene <npk@txtincorporated.com>
2016-10-19 01:54:23 -0700

    Test augmented functionality; still need to test figlet against error mssg

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,6 +1,6 @@
-var assert = require('assert');
+// var assert = require('assert');
 var greet = require('../lib/greet');
-// var assert = require('chai').assert;
+var assert = require('chai').assert;
 
 describe('greeter', function() {
 
@@ -9,20 +9,12 @@ describe('greeter', function() {
     var greeting = greet('tester');
     //use the assert module to test
     //will throw error for us if not true
-<<<<<<< HEAD
-    assert.equal(greeting, 'Hello tester.  ');
-=======
     assert.match(greeting, /^Hello tester\.  Today is/g);
->>>>>>> txtincFiglet
   });
 
   it('greets with "friend" when no name given', function() {
     var greeting = greet();
-<<<<<<< HEAD
-    assert.equal(greeting, 'Hello friend.  ');
-=======
     assert.match(greeting, /^Hello friend\.  Today is/g);
->>>>>>> txtincFiglet
   });
 });
 
