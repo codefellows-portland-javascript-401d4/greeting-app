cfc87ebcabcf7cd97fb55cbe90eccd07d0d0a5f9
Steven Jackson <Steven Jackson>
2016-10-19 08:51:05 -0700

    Just getting something turned in. I am still looking for a library, found one and am starting to get it into code

--- greet.js
+++ greet.js
@@ -1,10 +1,12 @@
+var nice = require('ascii-art');
+
 function greet(name) {
     var name = process.argv.pop();
     process.argv.push(name);
-    return('Hello, ' + name + '!')
+    return('Hello, ' + name + '!');
 };
 
-console.log(greet());
+console.log('figlet ' + greet());
 greet();
 
 module.exports = greet;
