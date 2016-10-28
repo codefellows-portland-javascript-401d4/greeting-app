b3a4646b966a4fb58e376925957a9889dd353c9c
mizutombo <thomas.shultz52@gmail.com>
2016-10-28 16:05:17 -0700

    added .eslintrc file ... debug ongoing

--- greet.js
+++ greet.js
@@ -2,14 +2,14 @@ const emoji = require('node-emoji');
 const prompt = require('prompt');
 
 function greet(name) {
-    prompt.start();
-    var name = prompt.get('What is your name?');
-    name = name || 'mon ami';
-    if (name != null) {
-        return 'Bonjour, ' + name + '. ' + emoji.emojify('I :heart: :coffee:');
-    }   else {
-            return 'Bonjour, ' + name + '. ' + emoji.get('flag-fr');
-        }
-};
+  prompt.start();
+  var name = prompt.get('What is your name?');
+  name = name || 'mon ami';
+  if (name != null) {
+    return 'Bonjour, ' + name + '. ' + emoji.emojify('I :heart: :coffee:');
+  }   else {
+    return 'Bonjour, ' + name + '. ' + emoji.get('flag-fr');
+  }
+}
 
 module.exports = greet;
--- test/test.js
+++ test/test.js
@@ -3,22 +3,22 @@ var assert = require('chai').assert;
 var greetemoji = require('node-emoji').greetemoji;
 
 describe('greeting test', function() {
-    
-    it('greets with a name', function() {
-        var greeting = greet('test');
-        assert.equal(greeting, 'Bonjour, test');
-    });
 
-    it('greets with "mon ami" without name', function() {
-        var greeting = greet();
-        assert.equal(greeting, 'Bonjour, mon ami');
-    });
+  it('greets with a name', function() {
+    var greeting = greet('test');
+    assert.equal(greeting, 'Bonjour, test');
+  });
 
-    it('ends greeting with appropriate emoji', function() {
-        var greeting = greet('testemoji');
-        var regex = /^emoji/;
+  it('greets with "mon ami" without name', function() {
+    var greeting = greet();
+    assert.equal(greeting, 'Bonjour, mon ami');
+  });
+
+  it('ends greeting with appropriate emoji', function() {
+    var greeting = greet('testemoji');
+    var regex = /^emoji/;
         // isolate node-emoji at end of greeting
-        var endemoji = greeting.replace(regex, '');  
-        assert.isAbove(greetemoji.indexOf(endemoji), 0);
-    });
+    var endemoji = greeting.replace(regex, '');
+    assert.isAbove(greetemoji.indexOf(endemoji), 0);
+  });
 });
