9051d61e8329758201d479f2f3868b8061490144
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-10-29 18:25:42 -0700

    Third test passing

--- test/greet-test.js
+++ test/greet-test.js
@@ -1,5 +1,7 @@
 var assert = require('chai').assert;
 var greet = require('../greet');
+const random = require('random-greetings');
+const dogs = require('dog-ascii-faces');
 
 it('greets user by name with a randomly-generated greeting', function() {
     var greeting = greet('Caitlin');
@@ -11,6 +13,7 @@ it('greets with "User" if name is not given', function() {
     assert.equal(greetingBasic, `${greetingBasic}`);
 });
 
-it.skip('displays a dog ascii face after greeting user', function() {
-    assert.equal();
+it('displays a dog ascii face after greeting user', function() {
+    var dog = greet(process.argv[2]) + dogs();
+    assert.equal(dog, `${dog}`);
 });
